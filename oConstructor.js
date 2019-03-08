6be3f2a1e3c05f1da46c2a0ff078905e07a91e3f
Adrian <adrianh.macairan@gmail.com>
2019-03-07 21:01:01 -0500

    class-07 constructor

--- oConstructor.js
+++ oConstructor.js
@@ -1,5 +1,6 @@
 //empty array
 let heroes = []
+let hours = ['6am', '7am', '9am', '10am', '11am', '12pm']
 
 //creating a constructor
 let Hero = function(name, minPower, maxPower, powerPerBattle) {
@@ -9,6 +10,7 @@ let Hero = function(name, minPower, maxPower, powerPerBattle) {
     this.powerPerBattle = powerPerBattle
 }
 
+//making a prototype
 Hero.prototype.powerPerHour = function(){
     let randomNumber = Math.ceil(Math.random() * (this.max - this.min) + this.min)
     return randomNumber * this.powerPerBattle
@@ -22,5 +24,23 @@ let hawkEye = new Hero('Hawkeye', 20, 100, 65)
 let spiderMan = new Hero('Spider Man', 100, 2500, 1200)
 let capAmerica = new Hero('Captain America', 200, 2000, 1100)
 let blackWidow = new Hero('Black Widow', 10, 90, 65)
+
+//pushing new instances to the constructor
 heroes.push(ironMan, thor, hulk, hawkEye, spiderMan, capAmerica, blackWidow)
 console.log(heroes)
+
+//creating a table via js
+let tbl = document.createElement('table')
+let tblRow = document.createElement('tr')
+let tblHeader = document.createElement('tr')
+let docBody = document.getElementById('docbody')
+let tblHeaderCell = document.createElement('th')
+docBody.appendChild(tbl)
+tbl.appendChild(tblHeader)
+
+
+for (let i = 0; i < hours.length; i++){
+    let tblHeaderCell = document.createElement('th')
+    tblHeader.appendChild('tblHeaderCell')
+    tblHeaderCell.innerText = hours[i].powerPerHour()
+}
